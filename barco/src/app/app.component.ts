e6556c8c78837442938f61e4b2e4a6efb9e46fb7
mi19her <milcaph.19@gmail.com>
2019-12-17 21:19:24 -0500

    service database

--- barco/src/app/app.component.ts
+++ barco/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Stitch, RemoteMongoClient, AnonymousCredential} from 'mongodb-stitch-browser-sdk';
+// import { Stitch, RemoteMongoClient, AnonymousCredential} from 'mongodb-stitch-browser-sdk';
+import { StitchService } from './stitch.service';
 
 @Component({
   selector: 'app-root',
@@ -7,24 +8,27 @@ import { Stitch, RemoteMongoClient, AnonymousCredential} from 'mongodb-stitch-br
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'barco';
-  table = [1, 2, 3, 4, 5, 6, 7];
-  tableA = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
 
+  constructor(private stitchService:StitchService){}
+  
+  
   ngOnInit(): void {
-  const client = Stitch.initializeDefaultAppClient('battleship-tixkw');
-  const db = client.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db('jugador');
+    
+    console.log(this.stitchService) 
+     console.log(this.stitchService.client.db('jugador'))
+  // const client = Stitch.initializeDefaultAppClient('battleship-tixkw');
+  // const db = client.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db('jugador');
 
-  client.auth.loginWithCredential(new AnonymousCredential()).then(user =>
-  db.collection('jugador').updateOne({owner_id: client.auth.user.id}, {$set: {number: 42}}, {upsert: true})
-  ).then(() =>
-  db.collection('jugador').find({owner_id: client.auth.user.id}, { limit: 100}).asArray()
-  ).then(docs => {
-    console.log('Found docs', docs);
-    console.log('[MongoDB Stitch] Connected to Stitch');
-  }).catch(err => {
-    console.error(err);
-  });
+  // client.auth.loginWithCredential(new AnonymousCredential()).then(user =>
+  // db.collection('jugador').updateOne({owner_id: client.auth.user.id}, {$set: {number: 42}}, {upsert: true})
+  // ).then(() =>
+  // db.collection('jugador').find({owner_id: client.auth.user.id}, { limit: 100}).asArray()
+  // ).then(docs => {
+  //   console.log('Found docs', docs);
+  //   console.log('[MongoDB Stitch] Connected to Stitch');
+  // }).catch(err => {
+  //   console.error(err);
+  // });
 }
 }
 
--- barco/src/app/inicio/inicio.component.ts
+++ barco/src/app/inicio/inicio.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// import { Stitch, RemoteMongoClient } from 'mongodb-stitch-browser-sdk';
+
 
 @Component({
   selector: 'app-inicio',
@@ -10,6 +12,22 @@ export class InicioComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // const client = Stitch.initializeDefaultAppClient('battleship-tixkw');
+    // const db = client.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db('jugador');
+    // const collection = db.collection("jugador");
+   
+   
+    // const newItem = {
+    //   "name": "Plastic Bricks",
+    //   "quantity": 10,
+    //   "category": "toys",
+    //   "reviews": [{ "username": "legolover", "comment": "These are awesome!" }]
+    // };
+    
+    // collection.insertOne(newItem)
+    //   .then(result => console.log(`Successfully inserted item with _id: ${result.insertedId}`))
+    //   .catch(err => console.error(`Failed to insert item: ${err}`))
+    //    console.log(newItem);
   }
-
+  
 }
