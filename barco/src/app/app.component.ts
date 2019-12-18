35c335da9e736240891fa69af939533d2a04c0ad
mi19her <milcaph.19@gmail.com>
2019-12-18 17:25:30 -0500

    estructura antigua

--- barco/src/app/app.component.ts
+++ barco/src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent implements OnInit {
   title = 'barco';
   table = [1, 2, 3, 4, 5, 6, 7];
   tableA = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+  batle = true;
 
   ngOnInit(): void {
   const client = Stitch.initializeDefaultAppClient('battleship-tixkw');
