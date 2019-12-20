c4b8d60626efbfa44c306366199a5f1ffec49f6f
melizasosa <s.m.meliza@gmail.com>
2019-12-20 10:22:01 -0500

    Almacenar las posiciones de bots en un array

--- barco/src/app/posicion-barcos/posicion-barcos.component.ts
+++ barco/src/app/posicion-barcos/posicion-barcos.component.ts
@@ -10,6 +10,9 @@ export class PosicionBarcosComponent implements OnInit {
   title = 'barco';
   table = [1, 2, 3, 4, 5, 6, 7];
   tableA = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+  aa=[];
+
+
 
   constructor() { }
 
@@ -17,7 +20,18 @@ export class PosicionBarcosComponent implements OnInit {
   }
 
   getPositionsBots(a, b) {
-    console.log(a, b);
+    const arrBots = [];
+ 
+    let stringBots=a+b;
+    const newArray=[];
+
+      arrBots.push(stringBots);
+    if(stringBots !=0){
+      // console.log(arrBots);
+      this.aa.push(stringBots);
+      }     
+    console.log(this.aa);
+    return this.aa;
   }
 
 }
